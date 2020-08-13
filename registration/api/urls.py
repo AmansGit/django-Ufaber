6b04750d2e5325fa90683523f48b80e44e98d2ba
AmansGit <amans1997a@gmail.com>
2020-08-13 20:53:27 +0530

    Clean Code

--- registration/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from registration.api.views import (
-		registration_view
-	)	
-app_name = "registration"
-
-urlpatterns = [
-	path('register', registration_view, name="register"),
-]
